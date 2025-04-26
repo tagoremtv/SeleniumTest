6eb64a1470ce62270bec5528f1406ab1e7b9288e
Tagore Varma Mallela <148947223+tagoremtv@users.noreply.github.com>
2025-04-26 13:13:21 +0530

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -3,15 +3,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
 
-def test_duckduckgo_search():
+def test_google_search():
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
     driver = webdriver.Chrome(options=options)
-    driver.get("https://duckduckgo.com")
-    
+    driver.get("https://www.google.com")
+
     search_box = driver.find_element(By.NAME, "q")
     search_box.send_keys("Azure DevOps Selenium")
     search_box.submit()
